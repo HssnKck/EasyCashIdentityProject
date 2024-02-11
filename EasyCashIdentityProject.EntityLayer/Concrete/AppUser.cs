9fc0806433bd96f70078361469ba36343b9f28ee
Hasan Küçük <Muhammedhasankucuk39@gmail.com>
2024-02-11 14:06:45 +0300

    11.02.24.14:06

--- EasyCashIdentityProject.EntityLayer/Concrete/AppUser.cs
+++ EasyCashIdentityProject.EntityLayer/Concrete/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace EasyCashIdentityProject.EntityLayer.Concrete
 {
-    public class AppUser
+    public class AppUser : IdentityUser<int>
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string ImageUrl { get; set; }
+
+
+
     }
 }
