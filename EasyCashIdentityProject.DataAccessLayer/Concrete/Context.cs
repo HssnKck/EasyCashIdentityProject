02014195a8460309999bd2387eccf2ff4ae79d0b
Hasan Küçük <Muhammedhasankucuk39@gmail.com>
2024-03-10 18:55:41 +0300

    10.03.24.18:55

--- EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -19,6 +19,21 @@ namespace EasyCashIdentityProject.DataAccessLayer.Concrete
         public DbSet<CustomerAccount> CustomerAccounts { get; set; }
         public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<CustomerAccountProcess>()
+                .HasOne(x => x.SenderCustomer)
+                .WithMany(y => y.CustomerSender)
+                .HasForeignKey(x => x.SenderID)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+            
+            builder.Entity<CustomerAccountProcess>()
+                .HasOne(x => x.ReceiverCustomer)
+                .WithMany(y => y.CustomerReceiver)
+                .HasForeignKey(x => x.ReceiverID    )
+                .OnDelete(DeleteBehavior.ClientSetNull);
+            base.OnModelCreating(builder);
+        }
 
 
     }
